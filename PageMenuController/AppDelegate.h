75c8a60e51db39f9ca0b1e3fe2f610d74ab2420c
MagickWorX <kouichi.abe.wall@magickworx.com>
2016-11-24 01:09:39 +0900

    Initial Commit

---
+++ PageMenuController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PageMenuController
+//
+//  Created by Kouichi ABE (WALL) on 2016/11/24.
+//  Copyright © 2016年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PageMenuController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PageMenuController
+//
+//  Created by Kouichi ABE (WALL) on 2016/11/24.
+//  Copyright © 2016年 Kouichi ABE (WALL). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
